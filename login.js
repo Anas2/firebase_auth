dd7d5f3b169f2c3e7b1b0fc3d21b8654905c4c59
Mohammad Anas <muhammadanasq11@gmail.com>
2022-11-12 12:25:20 +0500

    firebase auth

---
+++ login.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCVpAI-jh2QES25hyZvAu1vrX7ILhqT5zo",
+  authDomain: "anas-f5775.firebaseapp.com",
+  databaseURL: "https://anas-f5775.firebaseio.com",
+  projectId: "anas-f5775",
+  storageBucket: "anas-f5775.appspot.com",
+  messagingSenderId: "861907538096",
+  appId: "1:861907538096:web:3b46e6ef8a0b4fc6b9ab75",
+  measurementId: "G-RP65S58B7M"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+window.login= function(e) {
+  e.preventDefault();
+  var obj = {
+    email: email.value,
+    password: password.value,
+  };
+
+  signInWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function (success) {
+      console.log(success.user.uid);
+      window.location.replace('index.html')
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+
+  console.log(obj);
+}
